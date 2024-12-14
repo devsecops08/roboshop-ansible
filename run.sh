a80b848448c9d4b193d29a977704c9c85e14e431
Your Name <you@example.com>
2024-12-14 18:46:38 +0530

    add

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ if [ $# -ne 2 ]; then
   exit 1
   
 fi
-ansible-playbook -i $1-$2.adevsecops08.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e  app_name=$1 roboshop.yml
+ansible-playbook -i $1-$2.adevsecops08.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e  component=$1 roboshop.yml
